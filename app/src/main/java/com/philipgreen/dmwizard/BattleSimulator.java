32d4362deb544657f2296fa380022082a628b29c
Philip Green <philip.green@q2ebanking.com>
2016-07-31 18:02:33 -0500

    Initial commit

---
+++ app/src/main/java/com/philipgreen/dmwizard/BattleSimulator.java
@@ -0,0 +1,13 @@
+package com.philipgreen.dmwizard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BattleSimulator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle_simulator);
+    }
+}
