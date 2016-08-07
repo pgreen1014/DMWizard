c4e60b6b1a44954fc9ff626ff58413040769dbe3
Philip Green <philip.green@q2ebanking.com>
2016-08-07 09:46:06 -0500

    added Dagger class extending BaseWeapon

---
+++ app/src/main/java/com/philipgreen/dmwizard/dice/Dagger.java
@@ -0,0 +1,12 @@
+package com.philipgreen.dmwizard.dice;
+
+import com.philipgreen.dmwizard.weapons.BaseWeapon;
+
+/**
+ * Created by pgreen on 8/7/16.
+ */
+public class Dagger extends BaseWeapon {
+    public Dagger(int damageDie, int dieNumber) {
+        super(damageDie, dieNumber);
+    }
+}
