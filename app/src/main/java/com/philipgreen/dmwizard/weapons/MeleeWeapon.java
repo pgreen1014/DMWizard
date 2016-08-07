d10f198ebb5795c7a4dfb859e433378818dabd0f
Philip Green <philip.green@q2ebanking.com>
2016-08-07 12:09:18 -0500

    Created ranged and thrown weapon abstract class

--- app/src/main/java/com/philipgreen/dmwizard/weapons/MeleeWeapon.java
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/MeleeWeapon.java
@@ -3,9 +3,9 @@ package com.philipgreen.dmwizard.weapons;
 /**
  * Created by pgreen on 8/7/16.
  */
-public abstract class ThrownWeapon extends BaseWeapon {
+public abstract class MeleeWeapon extends BaseWeapon {
 
-    public ThrownWeapon(int damageDie, int dieNumber) {
+    public MeleeWeapon(int damageDie, int dieNumber) {
         super(damageDie, dieNumber);
     }
 }
