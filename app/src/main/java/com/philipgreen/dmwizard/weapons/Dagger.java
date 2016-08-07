03f2aed31e40eb2965dac9906ec5b11ca086b293
Philip Green <philip.green@q2ebanking.com>
2016-08-07 11:55:26 -0500

    Moved Dagger.java to weapons package from dice package

--- app/src/main/java/com/philipgreen/dmwizard/weapons/Dagger.java
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/Dagger.java
@@ -1,12 +1,10 @@
-package com.philipgreen.dmwizard.dice;
-
-import com.philipgreen.dmwizard.weapons.BaseWeapon;
-import com.philipgreen.dmwizard.weapons.WeaponType;
+package com.philipgreen.dmwizard.weapons;
 
 /**
  * Created by pgreen on 8/7/16.
  */
 public class Dagger extends BaseWeapon {
+
     public Dagger(int damageDie, int dieNumber) {
         super(damageDie, dieNumber, new WeaponType[] {WeaponType.MELEE, WeaponType.THROWN});
     }
