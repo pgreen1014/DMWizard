b34b8dda36967867336a1051b2d1a91358f38623
Philip Green <philip.green@q2ebanking.com>
2016-08-07 11:50:30 -0500

    added weapon types to super

--- app/src/main/java/com/philipgreen/dmwizard/dice/Dagger.java
+++ app/src/main/java/com/philipgreen/dmwizard/dice/Dagger.java
@@ -1,12 +1,13 @@
 package com.philipgreen.dmwizard.dice;
 
 import com.philipgreen.dmwizard.weapons.BaseWeapon;
+import com.philipgreen.dmwizard.weapons.WeaponType;
 
 /**
  * Created by pgreen on 8/7/16.
  */
 public class Dagger extends BaseWeapon {
     public Dagger(int damageDie, int dieNumber) {
-        super(damageDie, dieNumber);
+        super(damageDie, dieNumber, new WeaponType[] {WeaponType.MELEE, WeaponType.THROWN});
     }
 }
---
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/WeaponType.java
@@ -0,0 +1,7 @@
+package com.philipgreen.dmwizard.weapons;
+
+/**
+ * Created by pgreen on 8/7/16.
+ */
+public enum WeaponType {
+}
