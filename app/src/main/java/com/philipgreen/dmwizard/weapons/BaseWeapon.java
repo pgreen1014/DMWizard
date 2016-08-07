9835bbb8e1e5cd72d3cae075b515fb73ccc5390b
Philip Green <philip.green@q2ebanking.com>
2016-08-07 09:41:45 -0500

    added damageRoll function

--- app/src/main/java/com/philipgreen/dmwizard/weapons/BaseWeapon.java
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/BaseWeapon.java
@@ -1,7 +1,20 @@
 package com.philipgreen.dmwizard.weapons;
 
+import com.philipgreen.dmwizard.dice.Dice;
+
 /**
  * Created by pgreen on 8/7/16.
  */
 public abstract class BaseWeapon {
+    private int mDamageDie; // Die used for rolling damage
+    private int mDieNumber; // Number of damage die used
+
+    public BaseWeapon(int damageDie, int dieNumber) {
+        this.mDamageDie = damageDie;
+        this.mDieNumber = dieNumber;
+    }
+
+    public int damageRoll() {
+        return Dice.rollDice(mDamageDie, mDieNumber);
+    }
 }
