222d4d2b532d83b6671da440495834987fe8640f
Philip Green <philip.green@q2ebanking.com>
2016-08-07 18:06:12 -0500

    created button widget to test character creation

--- app/src/main/java/com/philipgreen/dmwizard/BattleSimulator.java
+++ app/src/main/java/com/philipgreen/dmwizard/BattleSimulator.java
@@ -2,12 +2,18 @@ package com.philipgreen.dmwizard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class BattleSimulator extends AppCompatActivity {
+    Button mBtnCreateCharacter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_simulator);
+
+        mBtnCreateCharacter = (Button) findViewById(R.id.character_create_button);
+
+        
     }
 }
