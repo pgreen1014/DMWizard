c1d70dab891f107ad9542eea2c7887f7df81fd3c
Philip Green <philip.green@q2ebanking.com>
2016-08-07 12:05:54 -0500

    Added abstract ThrownWeapon and extended Dagger from it

--- app/src/main/java/com/philipgreen/dmwizard/weapons/ThrownWeapon.java
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/ThrownWeapon.java
@@ -3,9 +3,9 @@ package com.philipgreen.dmwizard.weapons;
 /**
  * Created by pgreen on 8/7/16.
  */
-public class Dagger extends ThrownWeapon {
-
-    public Dagger(int damageDie, int dieNumber) {
+public abstract class ThrownWeapon extends BaseWeapon {
+    
+    public ThrownWeapon(int damageDie, int dieNumber) {
         super(damageDie, dieNumber);
     }
 }
