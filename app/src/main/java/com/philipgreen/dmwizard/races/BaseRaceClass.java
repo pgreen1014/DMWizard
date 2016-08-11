03d110df4b9cbb69bef6985b59581748ebba64f6
Philip Green <philip.green@q2ebanking.com>
2016-08-10 21:58:25 -0500

    Added abstract methods and member variables

--- app/src/main/java/com/philipgreen/dmwizard/races/BaseRaceClass.java
+++ app/src/main/java/com/philipgreen/dmwizard/races/BaseRaceClass.java
@@ -1,7 +1,31 @@
 package com.philipgreen.dmwizard.races;
 
+import com.philipgreen.dmwizard.data.BaseStats;
+import com.philipgreen.dmwizard.data.Languages;
+import com.philipgreen.dmwizard.weapons.BaseWeapon;
+
+import java.util.HashMap;
+
 /**
  * Created by pgreen on 8/10/16.
  */
-public class BaseRaceClass {
+public abstract class BaseRaceClass {
+    private int mSpeed;              // Base speed in feet
+    private BaseWeapon[] mWeaponProficiencies;
+    private Languages[] mLanguages;
+    private HashMap<BaseStats, Integer> mStatBonuses;
+
+
+    public BaseRaceClass() {
+        
+    }
+
+    /////////////////////////
+    //   ABSTRACT METHODS  //
+    /////////////////////////
+
+    public abstract int initSpeed();
+    public abstract BaseWeapon[] initWeaponProficiencies();
+    public abstract Languages[] initLanguages();
+    public abstract HashMap<BaseStats, Integer> initStatBonueses();
 }
