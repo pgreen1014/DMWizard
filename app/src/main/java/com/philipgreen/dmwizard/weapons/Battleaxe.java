0f5a95df144fad0784e64668efa060905a973c3e
Philip Green <philip.green@q2ebanking.com>
2016-08-10 22:35:31 -0500

    added weapons to weapons package

---
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/Battleaxe.java
@@ -0,0 +1,39 @@
+package com.philipgreen.dmwizard.weapons;
+
+import com.philipgreen.dmwizard.data.WeaponDamageType;
+import com.philipgreen.dmwizard.data.WeaponProperties;
+
+/**
+ * Created by pgreen on 8/10/16.
+ */
+public class Battleaxe extends MeleeWeapon {
+    @Override
+    public int initDamageDie() {
+        return 0;
+    }
+
+    @Override
+    public int initDieNumber() {
+        return 0;
+    }
+
+    @Override
+    public int initCost() {
+        return 0;
+    }
+
+    @Override
+    public WeaponDamageType initWeaponDamageType() {
+        return null;
+    }
+
+    @Override
+    public int initWeight() {
+        return 0;
+    }
+
+    @Override
+    public WeaponProperties[] initWeaponProperties() {
+        return new WeaponProperties[0];
+    }
+}
---
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/Handaxe.java
@@ -0,0 +1,49 @@
+package com.philipgreen.dmwizard.weapons;
+
+import com.philipgreen.dmwizard.data.WeaponDamageType;
+import com.philipgreen.dmwizard.data.WeaponProperties;
+
+/**
+ * Created by pgreen on 8/10/16.
+ */
+public class Handaxe extends ThrownWeapon {
+    @Override
+    public int initMinThrowRange() {
+        return 0;
+    }
+
+    @Override
+    public int initMaxThrowRange() {
+        return 0;
+    }
+
+    @Override
+    public int initDamageDie() {
+        return 0;
+    }
+
+    @Override
+    public int initDieNumber() {
+        return 0;
+    }
+
+    @Override
+    public int initCost() {
+        return 0;
+    }
+
+    @Override
+    public WeaponDamageType initWeaponDamageType() {
+        return null;
+    }
+
+    @Override
+    public int initWeight() {
+        return 0;
+    }
+
+    @Override
+    public WeaponProperties[] initWeaponProperties() {
+        return new WeaponProperties[0];
+    }
+}
