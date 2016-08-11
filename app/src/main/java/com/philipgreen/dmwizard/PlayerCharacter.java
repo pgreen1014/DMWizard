74942463f3727e4b2350def60982f8307c9c7bce
Philip Green <philip.green@q2ebanking.com>
2016-08-11 12:03:42 -0500

    added memeber variables

--- app/src/main/java/com/philipgreen/dmwizard/PlayerCharacter.java
+++ app/src/main/java/com/philipgreen/dmwizard/PlayerCharacter.java
@@ -1,7 +1,20 @@
 package com.philipgreen.dmwizard;
 
+import com.philipgreen.dmwizard.playerClasses.BasePlayerClass;
+import com.philipgreen.dmwizard.races.BaseRaceClass;
+import com.philipgreen.dmwizard.weapons.abstractWeapons.BaseWeapon;
+
+import java.util.ArrayList;
+
 /**
  * Created by pgreen on 8/11/16.
+ *
+ * This class will manage all of the aspects of playing a character and will hold onto
+ * data such as classes, race, feats and equipment.
  */
 public class PlayerCharacter {
+    private ArrayList<BasePlayerClass> mClasses;
+    private BaseRaceClass mRace;
+    private ArrayList<BaseWeapon> mWeapons;
+
 }
