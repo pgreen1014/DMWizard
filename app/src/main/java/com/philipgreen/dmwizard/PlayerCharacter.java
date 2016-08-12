10f6ce563fc512227695de4cd3e5de74220521cc
Philip Green <philip.green@q2ebanking.com>
2016-08-12 18:21:02 -0500

    added setUpSkills() to set character skill modifiers

--- app/src/main/java/com/philipgreen/dmwizard/PlayerCharacter.java
+++ app/src/main/java/com/philipgreen/dmwizard/PlayerCharacter.java
@@ -126,8 +126,10 @@ public class PlayerCharacter {
         this.mWisdom = wis;
         this.mCharisma = cha;
         this.mProficientSkills = setupSkillProficiencies(mClasses);
+        setUpSkills(mProficientSkills);
     }
 
+    // Grabs skill proficiencies from all classes to create an array list of all character proficiencies
     private ArrayList<Skills> setupSkillProficiencies(ArrayList<BasePlayerClass> classes) {
         ArrayList<Skills> combinedProficientSkills = new ArrayList<>();
 
@@ -141,6 +143,86 @@ public class PlayerCharacter {
         }
 
         return combinedProficientSkills;
+    }
+
+    // Set skill modifiers
+    private void setUpSkills(ArrayList<Skills> proficientSkills) {
+        this.mAcrobatics = getDexterityModifier();
+        this.mAnimalHandling = getWisdomModifier();
+        this.mArcana = getIntelligenceModifier();
+        this.mAthletics = getStrenthModifier();
+        this.mDeception = getCharismaModifier();
+        this.mHistory = getIntelligenceModifier();
+        this.mInsight = getWisdomModifier();
+        this.mIntimidation = getCharismaModifier();
+        this.mInvestigation = getIntelligenceModifier();
+        this.mMedicine = getWisdomModifier();
+        this.mNature = getIntelligenceModifier();
+        this.mPerception = getWisdomModifier();
+        this.mPerformance = getCharismaModifier();
+        this.mPersuasion = getCharismaModifier();
+        this.mReligion = getIntelligenceModifier();
+        this.mSleightOfHand = getDexterityModifier();
+        this.mStealth = getDexterityModifier();
+        this.mSurvival = getWisdomModifier();
+        for(Skills skill: proficientSkills) {
+            switch (skill) {
+                case ACROBATICS:
+                    this.mAcrobatics += mProficiencyBonus;
+                    break;
+                case ANIMAL_HANDLING:
+                    this.mAnimalHandling += mProficiencyBonus;
+                    break;
+                case ARCANA:
+                    this.mArcana += mProficiencyBonus;
+                    break;
+                case ATHLETICS:
+                    this.mAthletics += mProficiencyBonus;
+                    break;
+                case DECEPTION:
+                    this.mDeception += mProficiencyBonus;
+                    break;
+                case HISTORY:
+                    this.mHistory += mProficiencyBonus;
+                    break;
+                case INSIGHT:
+                    this.mInsight += mProficiencyBonus;
+                    break;
+                case INTIMIDATION:
+                    this.mIntimidation += mProficiencyBonus;
+                    break;
+                case INVESTIGATION:
+                    this.mInvestigation += mProficiencyBonus;
+                    break;
+                case MEDICINE:
+                    this.mMedicine += mProficiencyBonus;
+                    break;
+                case NATURE:
+                    this.mNature += mProficiencyBonus;
+                    break;
+                case PERCEPTION:
+                    this.mPerception += mProficiencyBonus;
+                    break;
+                case PERFORMANCE:
+                    this.mPerformance += mProficiencyBonus;
+                    break;
+                case PERSUASION:
+                    this.mPersuasion += mProficiencyBonus;
+                    break;
+                case RELIGION:
+                    this.mReligion += mProficiencyBonus;
+                    break;
+                case SLEIGHT_OF_HAND:
+                    this.mSleightOfHand += mProficiencyBonus;
+                    break;
+                case STEALTH:
+                    this.mStealth += mProficiencyBonus;
+                    break;
+                case SURVIVAL:
+                    this.mStealth += mProficiencyBonus;
+                    break;
+            }
+        }
     }
 
     /////////////////////////
