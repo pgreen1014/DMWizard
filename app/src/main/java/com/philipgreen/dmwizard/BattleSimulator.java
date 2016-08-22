118990956e0d8091daec4b1155a6eee3d240be13
Philip Green <philip.green@q2ebanking.com>
2016-08-22 02:07:22 -0500

    successful test case of PlayerCharacter.throwWeapon()

--- app/src/main/java/com/philipgreen/dmwizard/BattleSimulator.java
+++ app/src/main/java/com/philipgreen/dmwizard/BattleSimulator.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.philipgreen.dmwizard.data.BaseStats;
 import com.philipgreen.dmwizard.data.Skills;
 import com.philipgreen.dmwizard.dice.Dice;
 import com.philipgreen.dmwizard.playerClasses.Barbarian;
@@ -13,6 +14,7 @@ import com.philipgreen.dmwizard.playerClasses.BasePlayerClass;
 import com.philipgreen.dmwizard.races.BaseRaceClass;
 import com.philipgreen.dmwizard.races.Dwarf;
 import com.philipgreen.dmwizard.weapons.Dagger;
+import com.philipgreen.dmwizard.weapons.Warhammer;
 import com.philipgreen.dmwizard.weapons.abstractWeapons.BaseWeapon;
 
 import java.util.ArrayList;
@@ -35,11 +37,11 @@ public class BattleSimulator extends AppCompatActivity {
                 BasePlayerClass playerClass = new Barbarian(1, playerSkills);
                 BaseRaceClass playerRace = new Dwarf();
 
-                PlayerCharacter player = new PlayerCharacter(playerClass, playerRace, 17, 15, 14, 10, 13, 9);
+                PlayerCharacter player = new PlayerCharacter(playerClass, playerRace, 17, 14, 14, 10, 13, 9);
                 Log.i(TAG, player.toString());
 
-                BaseWeapon weapon = new Dagger();
-                int damageRoll = player.attack(weapon);
+                BaseWeapon dagger = new Dagger();
+                int damageRoll = player.throwAttack(dagger, BaseStats.DEXTERITY);
                 Log.i("BattleSimulator", "dagger damage roll: " + Integer.toString(damageRoll));
             }
         });
