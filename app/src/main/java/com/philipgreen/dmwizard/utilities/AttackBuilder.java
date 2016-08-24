1f8ef9a9a4cc2c078a5d4957fead96b2fdb52f47
Philip Green <philip.green@q2ebanking.com>
2016-08-23 23:35:06 -0500

    added empty constrcuter and Attacking Weapon member variable with setter

--- app/src/main/java/com/philipgreen/dmwizard/utilities/AttackBuilder.java
+++ app/src/main/java/com/philipgreen/dmwizard/utilities/AttackBuilder.java
@@ -1,7 +1,18 @@
 package com.philipgreen.dmwizard.utilities;
 
+import com.philipgreen.dmwizard.weapons.abstractWeapons.BaseWeapon;
+
 /**
  * Created by pgreen on 8/23/16.
  */
 public class AttackBuilder {
+    private BaseWeapon mAttackingWeapon;
+
+    public AttackBuilder() {
+
+    }
+
+    public void setWeapon(BaseWeapon weapon) {
+        mAttackingWeapon = weapon;
+    }
 }
