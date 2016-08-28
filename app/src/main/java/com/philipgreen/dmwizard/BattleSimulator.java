d02404939caf3b230ae83d78748e579c543aa3dc
Philip Green <philip.green@q2ebanking.com>
2016-08-28 10:46:03 -0500

    setup new test case

--- app/src/main/java/com/philipgreen/dmwizard/BattleSimulator.java
+++ app/src/main/java/com/philipgreen/dmwizard/BattleSimulator.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.philipgreen.dmwizard.battle.AttackBuilder;
 import com.philipgreen.dmwizard.data.BaseStats;
 import com.philipgreen.dmwizard.data.Skills;
 import com.philipgreen.dmwizard.playerClasses.Barbarian;
@@ -32,10 +33,16 @@ public class BattleSimulator extends AppCompatActivity {
                 Skills[] playerSkills = new Skills[] {Skills.ANIMAL_HANDLING, Skills.SURVIVAL};
                 BasePlayerClass playerClass = new Barbarian(1, playerSkills);
                 BaseRaceClass playerRace = new Dwarf();
+                PlayerCharacter player1 = new PlayerCharacter(playerClass, playerRace, 17, 14, 14, 10, 13, 9);
+                Log.i(TAG, player1.toString());
 
-                PlayerCharacter player = new PlayerCharacter(playerClass, playerRace, 17, 14, 14, 10, 13, 9);
-                Log.i(TAG, player.toString());
+                Skills[] player2Skills = new Skills[] {Skills.INTIMIDATION, Skills.NATURE};
+                BasePlayerClass player2Class = new Barbarian(1, player2Skills);
+                BaseRaceClass player2Race = new Dwarf();
+                PlayerCharacter player2 = new PlayerCharacter(player2Class, player2Race, 12, 13, 14, 15, 16, 18);
 
+                AttackBuilder attackBuilder = new AttackBuilder();
+                
 
 
             }
