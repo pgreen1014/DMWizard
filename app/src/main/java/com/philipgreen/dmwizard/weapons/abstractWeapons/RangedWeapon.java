85c64aee2eec2562ef422324c53bf83a20a0519a
Philip Green <philip.green@q2ebanking.com>
2016-08-30 21:00:31 -0500

    added ammunition member variable along with getters and setters

--- app/src/main/java/com/philipgreen/dmwizard/weapons/abstractWeapons/RangedWeapon.java
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/abstractWeapons/RangedWeapon.java
@@ -7,9 +7,26 @@ import com.philipgreen.dmwizard.weapons.abstractWeapons.BaseWeapon;
  * Created by pgreen on 8/7/16.
  */
 public abstract class RangedWeapon extends BaseWeapon {
+    private int mAmmunition;
 
     public RangedWeapon() {
         super();
         addWeaponProperty(WeaponProperties.RANGE);
+    }
+
+    public int getAmmunition() {
+        return mAmmunition;
+    }
+
+    public void setAmmunition(int ammunition) {
+        mAmmunition = ammunition;
+    }
+
+    public void addAmmunition(int ammunitionAmountBeingAdded) {
+        mAmmunition += mAmmunition;
+    }
+
+    public void useAmmunition() {
+        mAmmunition -= 1;
     }
 }
