559cf3264efeeb6c604a5a915b4f7f46ce7179be
Philip Green <philip.green@q2ebanking.com>
2016-09-05 20:32:26 -0500

    added property interfaces: base and throwable

---
+++ app/src/main/java/com/philipgreen/dmwizard/weapons/propertyInterfaces/BaseWeaponProperty.java
@@ -0,0 +1,10 @@
+package com.philipgreen.dmwizard.weapons.propertyInterfaces;
+
+import com.philipgreen.dmwizard.data.WeaponProperties;
+
+/**
+ * Created by pgreen on 9/5/16.
+ */
+public interface BaseWeaponPropertyInterface {
+    WeaponProperties getType();
+}
