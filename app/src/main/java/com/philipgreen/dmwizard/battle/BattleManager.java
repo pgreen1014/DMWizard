f36058304850ca50014e2a1f54e7903ac0d6a781
Philip Green <philip.green@q2ebanking.com>
2016-09-21 21:34:48 -0500

    added documentation, added setAttack() and clearAttack()

--- app/src/main/java/com/philipgreen/dmwizard/battle/BattleManager.java
+++ app/src/main/java/com/philipgreen/dmwizard/battle/BattleManager.java
@@ -3,24 +3,56 @@ package com.philipgreen.dmwizard.battle;
 import android.util.Log;
 
 import com.philipgreen.dmwizard.PlayerCharacter;
+import com.philipgreen.dmwizard.data.BaseStats;
 import com.philipgreen.dmwizard.data.WeaponProperties;
+import com.philipgreen.dmwizard.weapons.abstractWeapons.BaseWeapon;
 import com.philipgreen.dmwizard.weapons.abstractWeapons.RangedWeapon;
 
 /**
  * Created by pgreen on 8/25/16.
  *
- * This class is responsible for taking an attack object and executing the various kinds of attacks
+ * This class is responsible for taking an attack object and executing the various kinds of attacks. Attack is initially set
+ * in the constructor. In order to handle a new attack, a new BattleManager object can be made or simply call .setAttack(Attack arg)
+ * to pass a new attack object. After an attack has been executed .clearAttack() is called so that already executed attack is not
+ * accidentally executed again. A new attack will need to be passed in with .setAttack(Attack arg).
  */
 public class BattleManager {
     private static final String TAG = "BattleManager";
     private boolean mAttackSuccessful;
-    private PlayerCharacter mAttacker;
-    private PlayerCharacter mDefender;
 
-    public BattleManager() {}
+    private BaseWeapon mAttackingWeapon;
+    private PlayerCharacter mDefender;
+    private PlayerCharacter mAttacker;
+    private BaseStats mAttackModifierStat;
+    private int mPlayerDistance;
+    private boolean mTwoHandedAttack;
+    private boolean mOffHandWeaponAttack;
+    private AttackBuilder.AttackType mAttackType;
+
+    public BattleManager(Attack attack) {
+        setAttack(attack);
+    }
 
     public void setAttack(Attack attack) {
-        
+        mAttackingWeapon = attack.getAttackingWeapon();
+        mDefender = attack.getDefender();
+        mAttacker = attack.getAttacker();
+        mAttackModifierStat = attack.getAttackModifierStat();
+        mPlayerDistance = attack.getPlayerDistance();
+        mTwoHandedAttack = attack.isTwoHandedAttack();
+        mOffHandWeaponAttack = attack.isOffHandWeaponAttack();
+        mAttackType = attack.getAttackType();
+    }
+
+    private void clearAttack() {
+        mAttackingWeapon = null;
+        mDefender = null;
+        mAttacker = null;
+        mAttackModifierStat = null;
+        mPlayerDistance = 0;
+        mTwoHandedAttack = false;
+        mOffHandWeaponAttack = false;
+        mAttackType = null;
     }
 
     public void executeAttack(Attack attack) {
@@ -50,6 +82,7 @@ public class BattleManager {
         mDefender.takeDamage(damage);
 
         Log.i(TAG, "Defender now has " + Integer.toString(mDefender.getHitPoints()) + " hit points, ouch!");
+        clearAttack();
     }
 
     private int executeMeleeAttack(Attack attack) {
