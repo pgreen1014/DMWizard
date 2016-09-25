f5ecbd0b087a48978df5e67fb1e0d08c7bca4adf
Philip Green <philip.green@q2ebanking.com>
2016-09-25 14:28:12 -0500

    created method for casting to RangedWeapon

--- app/src/main/java/com/philipgreen/dmwizard/utils/SafeWeaponCaster.java
+++ app/src/main/java/com/philipgreen/dmwizard/utils/SafeWeaponCaster.java
@@ -2,6 +2,7 @@ package com.philipgreen.dmwizard.utils;
 
 import com.philipgreen.dmwizard.weapons.abstractWeapons.BaseWeapon;
 import com.philipgreen.dmwizard.weapons.abstractWeapons.MeleeWeapon;
+import com.philipgreen.dmwizard.weapons.abstractWeapons.RangedWeapon;
 
 /**
  * Created by pgreen on 9/25/16.
@@ -13,8 +14,15 @@ public class SafeWeaponCaster {
 
     public static MeleeWeapon castToMelee(BaseWeapon baseWeapon) {
         if(!(baseWeapon instanceof MeleeWeapon)) {
-            throw new IllegalArgumentException("weapon is not instance of MeleeWeapon");
+            throw new ClassCastException("weapon is not instance of MeleeWeapon");
         }
         return (MeleeWeapon) baseWeapon;
+    }
+
+    public static RangedWeapon castToRangedWeapon(BaseWeapon baseWeapon) {
+        if(!(baseWeapon instanceof RangedWeapon)) {
+            throw new ClassCastException("weapon is not instance of RangedWeapon");
+        }
+        return (RangedWeapon) baseWeapon;
     }
 }
