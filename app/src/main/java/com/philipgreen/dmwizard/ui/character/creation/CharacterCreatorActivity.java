967d441e92117561fe77c77d4af08a5bd301c56f
Philip Green <philip.green@q2ebanking.com>
2016-10-31 00:46:57 -0500

    made CharacterCreatorActivity a FragmentActivity

--- app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
+++ app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
@@ -1,11 +1,11 @@
 package com.philipgreen.dmwizard.ui.character.creation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.philipgreen.dmwizard.R;
 
-public class CharacterCreatorActivity extends AppCompatActivity {
+public class CharacterCreatorActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
