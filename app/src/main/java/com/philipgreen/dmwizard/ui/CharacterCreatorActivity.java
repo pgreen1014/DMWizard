b5b3bd40b8f8945143ee8d1c32f815bd41d5ab22
Philip Green <philip.green@q2ebanking.com>
2016-10-31 00:23:11 -0500

    created CharacterCreatorActivity

---
+++ app/src/main/java/com/philipgreen/dmwizard/ui/CharacterCreatorActivity.java
@@ -0,0 +1,15 @@
+package com.philipgreen.dmwizard.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.philipgreen.dmwizard.R;
+
+public class CharacterCreatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_creator);
+    }
+}
