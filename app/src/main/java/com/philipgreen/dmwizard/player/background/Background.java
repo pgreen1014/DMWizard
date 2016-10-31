9110476e061817dffce13e7e8f10dc19c40fabc6
Philip Green <philip.green@q2ebanking.com>
2016-10-30 23:45:56 -0500

    created interface abstractions for player backgrounds to inherit from

---
+++ app/src/main/java/com/philipgreen/dmwizard/player/background/Background.java
@@ -0,0 +1,10 @@
+package com.philipgreen.dmwizard.player.background;
+
+/**
+ * Created by pgreen on 10/30/16.
+ *
+ * Base interface from which PlayerCharacter backgrounds will inherit
+ */
+
+public interface Background {
+}
