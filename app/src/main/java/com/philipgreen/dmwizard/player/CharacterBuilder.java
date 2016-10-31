386e7bbebf5525da86a682f7d850d6859f904a9b
Philip Green <philip.green@q2ebanking.com>
2016-10-31 00:11:38 -0500

    created basic getters and setters with corresponding unit tests

--- app/src/main/java/com/philipgreen/dmwizard/player/CharacterBuilder.java
+++ app/src/main/java/com/philipgreen/dmwizard/player/CharacterBuilder.java
@@ -1,6 +1,11 @@
 package com.philipgreen.dmwizard.player;
 
+import com.philipgreen.dmwizard.player.background.Background;
+import com.philipgreen.dmwizard.player.utils.Skills;
+import com.philipgreen.dmwizard.playerClasses.BasePlayerClass;
 import com.philipgreen.dmwizard.races.BaseRaceClass;
+
+import java.util.ArrayList;
 
 /**
  * Created by pgreen on 10/30/16.
@@ -10,14 +15,107 @@ import com.philipgreen.dmwizard.races.BaseRaceClass;
 
 public class CharacterBuilder {
     private BaseRaceClass mRace;
+    private BasePlayerClass mClass;
+    private Background mBackground;
+    private int mStrength;
+    private int mDexterity;
+    private int mConstitution;
+    private int mIntelligence;
+    private int mWisdom;
+    private int mCharisma;
+    private ArrayList<Skills> mSkills;
 
     public CharacterBuilder setRace(BaseRaceClass playerRace) {
         mRace = playerRace;
         return this;
     }
 
+    public CharacterBuilder setClass(BasePlayerClass playerClass) {
+        mClass = playerClass;
+        return this;
+    }
+
+    public CharacterBuilder setBackground(Background background) {
+        mBackground = background;
+        return this;
+    }
+
+    public CharacterBuilder setStrength(int str) {
+        mStrength = str;
+        return this;
+    }
+
+    public CharacterBuilder setDexterity(int dex) {
+        mDexterity = dex;
+        return this;
+    }
+
+    public CharacterBuilder setConstitution(int con) {
+        mConstitution = con;
+        return this;
+    }
+
+    public CharacterBuilder setIntelligence(int intelligence) {
+        mIntelligence = intelligence;
+        return this;
+    }
+
+    public CharacterBuilder setWisdom(int wis) {
+        mWisdom = wis;
+        return this;
+    }
+
+    public CharacterBuilder setCharisma(int cha) {
+        mCharisma = cha;
+        return this;
+    }
+
+    public CharacterBuilder setSkills(ArrayList<Skills> skills) {
+        mSkills = skills;
+        return this;
+    }
+
+    //////////////////////////////////////
+    //             GETTERS              //
+    //////////////////////////////////////
+
     public BaseRaceClass getRace() {
         return mRace;
     }
 
+    public BasePlayerClass getPlayerClass() {
+        return mClass;
+    }
+
+    public Background getBackground() {
+        return mBackground;
+    }
+
+    public int getStrength() {
+        return mStrength;
+    }
+
+    public int getDexterity() {
+        return mDexterity;
+    }
+
+    public int getConstitution() {
+        return mConstitution;
+    }
+
+    public int getIntelligence() {
+        return mIntelligence;
+    }
+
+    public int getWisdom() {
+        return mWisdom;
+    }
+
+    public int getCharisma() {
+        return mCharisma;
+    }
+
+    public ArrayList<Skills> getSkills() {
+        return mSkills;
+    }
 }
--- app/src/test/java/com/philipgreen/dmwizard/CharacterBuilderTest.java
+++ app/src/test/java/com/philipgreen/dmwizard/CharacterBuilderTest.java
@@ -1,9 +1,14 @@
 package com.philipgreen.dmwizard;
 
 import com.philipgreen.dmwizard.player.CharacterBuilder;
+import com.philipgreen.dmwizard.player.utils.Skills;
+import com.philipgreen.dmwizard.playerClasses.Barbarian;
 import com.philipgreen.dmwizard.races.Dwarf;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -13,12 +18,54 @@ import static junit.framework.Assert.assertTrue;
  * Test for CharacterBuilder.java
  */
 
+ //// TODO: 10/30/16 add test for backgrounds when completed
+
 public class CharacterBuilderTest {
-    CharacterBuilder mBuilder = new CharacterBuilder();
+    private CharacterBuilder mBuilder;
+
+    @Before
+    public void init() {
+        mBuilder = new CharacterBuilder();
+    }
 
     @Test
     public void setPlayerRaceTest() {
         mBuilder.setRace(new Dwarf());
         assertTrue(mBuilder.getRace() instanceof Dwarf);
+    }
+
+    @Test
+    public void setPlayerClassTest() {
+        mBuilder.setClass(new Barbarian(1, new Skills[]{Skills.ATHLETICS, Skills.ANIMAL_HANDLING}));
+        assertTrue(mBuilder.getPlayerClass() instanceof Barbarian);
+    }
+
+    @Test
+    public void setCharacterAbilitiesTest() {
+        mBuilder.setStrength(10)
+                .setDexterity(11)
+                .setConstitution(12)
+                .setIntelligence(13)
+                .setWisdom(14)
+                .setCharisma(15);
+
+        assertTrue(mBuilder.getStrength() == 10);
+        assertTrue(mBuilder.getDexterity() == 11);
+        assertTrue(mBuilder.getConstitution() == 12);
+        assertTrue(mBuilder.getIntelligence() == 13);
+        assertTrue(mBuilder.getWisdom() == 14);
+        assertTrue(mBuilder.getCharisma() == 15);
+    }
+
+    @Test
+    public void setSkillsTest() {
+        ArrayList<Skills> skills = new ArrayList<>();
+        skills.add(0, Skills.ANIMAL_HANDLING);
+        skills.add(1, Skills.ATHLETICS);
+        mBuilder.setSkills(skills);
+
+        ArrayList<Skills> setSkills = mBuilder.getSkills();
+        assertTrue(setSkills.get(0) == Skills.ANIMAL_HANDLING);
+        assertTrue(setSkills.get(1) == Skills.ATHLETICS);
     }
 }
