a08287e863693250d2032d076d95d61fae3cde9f
Philip Green <philip.green@q2ebanking.com>
2016-10-31 00:23:59 -0500

    moved MainActivity to the ui package

--- app/src/main/java/com/philipgreen/dmwizard/ui/MainActivity.java
+++ app/src/main/java/com/philipgreen/dmwizard/ui/MainActivity.java
@@ -1,10 +1,11 @@
-package com.philipgreen.dmwizard;
+package com.philipgreen.dmwizard.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.philipgreen.dmwizard.R;
 
 
 public class MainActivity extends AppCompatActivity {
