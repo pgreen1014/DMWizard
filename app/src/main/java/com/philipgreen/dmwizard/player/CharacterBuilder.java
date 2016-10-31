ca4649bf152712ae1307344ead0278e6c89cb36e
Philip Green <philip.green@q2ebanking.com>
2016-10-30 23:32:50 -0500

    added documentation

--- app/src/main/java/com/philipgreen/dmwizard/player/CharacterBuilder.java
+++ app/src/main/java/com/philipgreen/dmwizard/player/CharacterBuilder.java
@@ -2,6 +2,8 @@ package com.philipgreen.dmwizard.player;
 
 /**
  * Created by pgreen on 10/30/16.
+ *
+ * Builder class responsible for dynamically building a PlayerCharacter.
  */
 
 public class CharacterBuilder {
