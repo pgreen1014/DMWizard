bd4c45728b4815af3542a20bf8b03262a13dd3c2
Philip Green <philip.green@q2ebanking.com>
2016-10-31 00:52:49 -0500

    made create character button start CharacterCreatorActivity

--- app/src/main/java/com/philipgreen/dmwizard/ui/MainActivity.java
+++ app/src/main/java/com/philipgreen/dmwizard/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package com.philipgreen.dmwizard.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.philipgreen.dmwizard.R;
+import com.philipgreen.dmwizard.ui.character.creation.CharacterCreatorActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         mBtnCreateCharacter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i = new Intent(MainActivity.this, CharacterCreatorActivity.class);
+                startActivity(i);
             }
         });
     }
