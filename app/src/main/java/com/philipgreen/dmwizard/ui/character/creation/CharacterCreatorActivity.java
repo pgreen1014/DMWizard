e0560a138b0ea71203204c581bf92326c8cbe4e4
Philip Green <philip.green@q2ebanking.com>
2016-10-31 01:14:00 -0500

    added code to start PageOneFragment in onCreate()

--- app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
+++ app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
@@ -1,7 +1,9 @@
 package com.philipgreen.dmwizard.ui.character.creation;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 import com.philipgreen.dmwizard.R;
 
@@ -11,5 +13,15 @@ public class CharacterCreatorActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_creator);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer_characterCreator);
+
+        if (fragment == null) {
+            fragment = new PageOneFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer_characterCreator, fragment)
+                    .commit();
+        }
     }
 }
