d8b2b7b5cdee99531905f3799b1bbfaf294b49aa
Philip Green <philip.green@q2ebanking.com>
2016-10-31 23:47:26 -0500

    added race and player enums

---
+++ app/src/main/java/com/philipgreen/dmwizard/playerClasses/utils/PlayerClassEnum.java
@@ -0,0 +1,9 @@
+package com.philipgreen.dmwizard.playerClasses.utils;
+
+/**
+ * Created by pgreen on 10/31/16.
+ */
+
+public enum PlayerClassEnum {
+    BARBARIAN, BARD, CLERIC, DRUID, FIGHTER, MONk, PALADIN, RANGER, ROGUE, SORCERER, WARLOCK, WIZARD
+}
---
+++ app/src/main/java/com/philipgreen/dmwizard/races/utils/RaceListEnum.java
@@ -0,0 +1,11 @@
+package com.philipgreen.dmwizard.races.utils;
+
+/**
+ * Created by pgreen on 10/31/16.
+ */
+
+public enum RaceListEnum {
+    DWARF, HILL_DWARF, MOUNTAIN_DWARF, ELF, HIGH_ELF, DARK_ELF, WOOD_ELF,
+    HALFLING, HUMAN, DRAGONBORN, GNOME, FOREST_GNOME, ROCK_GNOME, HALF_ELF,
+    HALF_ORC, TIEFLING
+}
