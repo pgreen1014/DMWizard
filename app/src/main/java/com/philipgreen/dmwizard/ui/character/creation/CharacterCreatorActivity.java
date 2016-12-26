1eaaec4594e52e91980076e9a0f9254a0dcc9dbe
Philip Green <philip.green@q2ebanking.com>
2016-12-25 22:48:08 -0600

    wired up ViewPager for character creation

--- app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
+++ app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
@@ -1,13 +1,50 @@
 package com.philipgreen.dmwizard.ui.character.creation;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class CharacterCreatorActivity extends CharacterCreationFragmentActivity {
+import com.philipgreen.dmwizard.R;
+
+import java.util.ArrayList;
+
+public class CharacterCreatorActivity extends FragmentActivity {
+
+    private final ArrayList<Fragment> characterCreationFragmentList = new ArrayList<Fragment>() {
+        {
+            add(new RacePickerFragment());
+            add(new ClassPickerFragment());
+        }
+    };
 
     @Override
-    protected Fragment createFragment() {
-        return new RacePickerFragment();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_creator);
+
+        CharacterCreatorPagerAdapter pagerAdapter = new CharacterCreatorPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager_character_creator);
+        viewPager.setAdapter(pagerAdapter);
     }
 
+    public class CharacterCreatorPagerAdapter extends FragmentPagerAdapter {
+
+        public CharacterCreatorPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return characterCreationFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return characterCreationFragmentList.size();
+        }
+    }
 
 }
