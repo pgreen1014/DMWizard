8ddf188c2136a0fac1bdc3b246c2d1156067c1e8
Philip Green <philip.green@q2ebanking.com>
2016-12-30 00:10:11 -0600

    implemented TabLayout with ViewPager for CharacterCreation

--- app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
+++ app/src/main/java/com/philipgreen/dmwizard/ui/character/creation/CharacterCreatorActivity.java
@@ -1,52 +1,126 @@
 package com.philipgreen.dmwizard.ui.character.creation;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.philipgreen.dmwizard.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CharacterCreatorActivity extends AppCompatActivity {
 
-    private final ArrayList<Fragment> characterCreationFragmentList = new ArrayList<Fragment>() {
-        {
-            add(new RacePickerFragment());
-            add(new ClassPickerFragment());
-        }
-    };
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_creator);
 
-        getSupportActionBar().setTitle("Character Creation");
+        mToolbar = (Toolbar) findViewById(R.id.charcter_creator_toolbar);
+        setSupportActionBar(mToolbar);
 
-        CharacterCreatorPagerAdapter pagerAdapter = new CharacterCreatorPagerAdapter(getSupportFragmentManager());
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager_character_creator);
-        viewPager.setAdapter(pagerAdapter);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mViewPager = (ViewPager) findViewById(R.id.character_creator_view_pager);
+        setupViewPager(mViewPager);
+
+        mTabLayout = (TabLayout) findViewById(R.id.character_creator_tab_layout);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
-    public class CharacterCreatorPagerAdapter extends FragmentPagerAdapter {
+    private void  setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new RacePickerFragment(), "Race");
+        adapter.addFrag(new ClassPickerFragment(), "Class");
+        viewPager.setAdapter(adapter);
+    }
 
-        public CharacterCreatorPagerAdapter(FragmentManager fm) {
-            super(fm);
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
 
         @Override
         public Fragment getItem(int position) {
-            return characterCreationFragmentList.get(position);
+            return mFragmentList.get(position);
         }
 
         @Override
         public int getCount() {
-            return characterCreationFragmentList.size();
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
         }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
