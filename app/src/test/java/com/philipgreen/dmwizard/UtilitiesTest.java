f1652874b6dfe3db97bf68678887edcee8864a7a
Philip Green <philip.green@q2ebanking.com>
2017-03-05 14:00:25 -0600

    updated tests with enum conversion

--- app/src/test/java/com/philipgreen/dmwizard/UtilitiesTest.java
+++ app/src/test/java/com/philipgreen/dmwizard/UtilitiesTest.java
@@ -1,8 +1,15 @@
 package com.philipgreen.dmwizard;
 
+import com.philipgreen.dmwizard.playerClasses.utils.PlayerClassEnum;
+import com.philipgreen.dmwizard.races.utils.RaceListManager;
 import com.philipgreen.dmwizard.utils.Dice;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -29,6 +36,38 @@ public class UtilitiesTest {
             int rollResult = Dice.rollDice(2, 6);
             assertTrue(rollResult >= 1 && rollResult <= 12);
         }
+    }
+
+    /**
+     * Tests that the RaceListEnum string conversion matches the correct UI presentation strings
+     *
+     */
+    @Test
+    public void raceListEnumReturnsStringListCorrectly() {
+        ArrayList<String> correctRaceListStrings = new ArrayList<>(Arrays.asList("Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half Elf", "Half Orc", "Tiefling"));
+
+        ArrayList<String> raceListEnum = RaceListManager.getRaceListForUIPresentation();
+        raceListEnum.removeAll(correctRaceListStrings);
+
+        assertTrue("RaceListEnum string conversion does not match proper UI presentation", raceListEnum.size() == 0);
+    }
+
+    /**
+     * Tests that PlayerClassEnum toString() returns correct String for UI
+     */
+    @Test
+    public void classListEnumReturnsCorrectUIStrings() {
+        ArrayList<String> correctClassListStrings = new ArrayList<>(Arrays.asList("Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"));
+
+        List<String> classList = new ArrayList<>();
+
+        for (PlayerClassEnum classEnum: PlayerClassEnum.values()) {
+            classList.add(classEnum.toString());
+        }
+
+        classList.removeAll(correctClassListStrings);
+
+        assertTrue("ClassListEnum string conversion does not match proper UI presentation", classList.size() == 0);
     }
 
 }
