5939a155de0b3db15791f185ab4ee9665a4b2ee7
Philip Green <philip.green@q2ebanking.com>
2017-04-07 12:08:47 -0500

    added import

--- app/src/main/java/com/philipgreen/dmwizard/ui/MainActivity.java
+++ app/src/main/java/com/philipgreen/dmwizard/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.philipgreen.dmwizard.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
